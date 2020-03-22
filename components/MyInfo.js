dddd3da8caf6aa2fb62428536fd412593127da0a
Sarthak Bhatnagar <Sarthak>
2020-03-22 12:17:59 -0700

    react app

---
+++ components/MyInfo.js
@@ -0,0 +1,26 @@
+// must be imported everywhere JSX will be used
+import React from "react"
+
+function MyInfo() {
+
+    return (
+      <div>
+        <h1>Sarthak Bhatnagar</h1>
+        <p>Learning React is fun.</p>
+        <ul>
+          <li>
+            Greece
+          </li>
+          <li>
+            Spain
+          </li>
+          <li>
+            Japan
+          </li>
+        </ul>
+      </div>
+    )
+}
+
+// ES6 syntax
+export default MyInfo
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+
+// import assumes the file is default js
+import MyInfo from '../components/MyInfo'
+
+ReactDOM.render(<MyInfo/>, document.getElementById("root")); 
